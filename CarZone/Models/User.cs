a40f138c2c41bbeec32adc362dbb380be923a3c0
OMKAR JOSHI <omkarjoshi1187@gmail.com>
2025-03-06 19:09:56 +0530

    Revert "Code changes backend work"
    This reverts commit 048a8c0ad67c595b8a2379465e603f03becbc643.

--- CarZone/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CarZone.Models
-{
-    public class User
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
-    }
-}
